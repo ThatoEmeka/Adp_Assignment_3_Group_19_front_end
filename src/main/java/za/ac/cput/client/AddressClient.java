cf29b4dd14c2950e7397a5c316d893d1cdf8c895
Shyam Prag <pragshyam11@gmail.com>
2021-10-24 23:07:30 +0200

    fixed address ting

--- src/main/java/za/ac/cput/client/AddressClient.java
+++ src/main/java/za/ac/cput/client/AddressClient.java
@@ -7,22 +7,22 @@ public class AddressClient {
     private RestClient client = new RestClient();
 
     public Address createAddress(Address address) {
-        return client.post("http://localhost:8090/Address/create", address, Address.class);
+        return client.post("http://localhost:8080/Address/create", address, Address.class);
     }
 
     public Address readAddress(String id) {
-        return client.get("http://localhost:8090/Address/read/" + id, Address.class);
+        return client.get("http://localhost:8080/Address/read/" + id, Address.class);
     }
 
     public Address updateAddress(Address address) {
-        return client.post("http://localhost:8090/Address/update", address, Address.class);
+        return client.post("http://localhost:8080/Address/update", address, Address.class);
     }
 
     public Boolean deleteAddress(String id) {
-        return client.get("http://localhost:8090/Address/delete/" + id, Boolean.class);
+        return client.get("http://localhost:8080/Address/delete/" + id, Boolean.class);
     }
 
     public Set<Address> getAllAddresses() {
-        return client.get("http://localhost:8090/Address/getall", new HashSet<Address>().getClass());
+        return client.get("http://localhost:8080/Address/getall", new HashSet<Address>().getClass());
     }
 }
