045152893db22c0d22b783de4471be828b4331da
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-24 19:29:15 +0200

    Removed unused imports

--- src/main/java/za/ac/cput/gui/customer/ViewCustomer.java
+++ src/main/java/za/ac/cput/gui/customer/ViewCustomer.java
@@ -1,14 +1,11 @@
 package za.ac.cput.gui.customer;
 
-import antlr.StringUtils;
-import org.json.JSONObject;
 import za.ac.cput.client.CustomerClient;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 
 public class ViewCustomer extends JFrame implements ActionListener {
     JFrame jFrame;
